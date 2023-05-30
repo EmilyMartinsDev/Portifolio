888ce817abc72127a32194bbde46c9af11d0d681
emily <emilycaroline01042005@gmail.com>
2023-05-30 15:44:07 -0300

    criando a sessao de projetos

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,17 @@
+import { Card, LinkBotao } from './styles'
+import Paragrafo from '../Paragrafo'
+import Titulo from '../Titulo'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projetos1</Titulo>
+    <Paragrafo tipo="secundaria">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam facere
+      necessitatibus nobis ipsam incidunt consequatur, ab qui quia sint
+      cupiditate culpa repudiandae porro dolores nulla, aliquid fuga laudantium,
+      alias eveniet.
+    </Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,26 @@
+import Projeto from '../../components/Projeto'
 import Titulo from '../../components/Titulo'
-
+import { ListContainer } from './styles'
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
+    <ListContainer>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </ListContainer>
   </section>
 )
 
